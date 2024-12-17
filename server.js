35495d81c81bc4a6eb41c0607332ebc5df041924
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-17 12:39:31 +0100

    progress

--- server.js
+++ server.js
@@ -1,15 +1,5 @@
 const { ApolloServer } = require("apollo-server");
-const { gql } = require("apollo-server");
-
-const typeDefs = gql`
-  type Book {
-    title: String
-    author: String
-  }
-  type Query {
-    books: [Book]
-  }
-`;
+const typeDefs = require("./schema/type-defs");
 
 const books = [
     {
