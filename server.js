2e453ecabcf3a6ba1243d154ada44b11003c5661
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-17 12:43:30 +0100

    progress

--- server.js
+++ server.js
@@ -1,13 +1,7 @@
 const { ApolloServer } = require("apollo-server");
+
 const typeDefs = require("./schema/type-defs");
-const books = require("./data/books");
-
-
-  const resolvers = {
-    Query: {
-      books: () => books,
-    },
-  };
+const resolvers = require("./schema/resolvers");
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
